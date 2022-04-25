a3b4ac1c7185cbc8a531955ca8372a4f2bb190f1
jason <jason.schermer@hva.nl>
2022-04-26 01:01:32 +0200

    adding some text wip

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
+import StyledHome from './home.styled';
 
 function Home() {
   const [open, setOpen] = useState(false);
 
   return (
-    <div>Hello</div>
+    <div className="container"><h2>Hello, my name is Jason Schermer and I am a Software Engineering Student</h2></div>
   );
 }
 
